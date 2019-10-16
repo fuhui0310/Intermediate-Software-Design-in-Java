8ad02f3774e2b3eb994dee45779d77603c41d407
Fu <evanhui0310@gmail.com>
2019-10-16 01:48:57 -0700

    project3 update

---
+++ java_project_3/README.txt
@@ -0,0 +1,31 @@
+project folder: fuhui0310-project04\
+
+Brief description of submitted files
+
+src\cellularDate\Country.java
+ - One object  contains the name of the country and the list of subscriptions.
+
+src\cellularDate\CSVReader.java
+ - One object read the .csv file one line at a time and set various attributes.
+
+src\cellularDate\Subscription.java
+ - One object contains the specified year and its subscription data.
+
+src\cellularDate\TestCountry.java
+ - Tests the Country class by creating multiple objects.
+ - Creates one object of type CSVReader class, which reads input from a CSV file. Uses
+ - the attributes stored in CSVReader object to create objects of type Country class.
+
+resources\cellular.csv
+ - A CSV (Comma Separated Value) file.
+ - First row contains the names of country.
+ - Lines 2 to EOF (end of file) contain the cellular data over many years for different countries.
+
+resources\cellular_short_oneDecade.csv
+  - A CSV (Comma Separated Value) file.
+ - First row contains the names of three country.
+ - Lines 2 to EOF (end of file) contain the cellular data over ten years for four countries.
+
+RUN.txt - console output of TestCountry.java.
+
+README.txt - description of submitted files
---
+++ java_project_3/src/cellularData/SubscriptionYear.java
@@ -0,0 +1,59 @@
+package cellularData;
+
+/**
+ * One object contains the specified year and its subscription data.
+ */
+public class SubscriptionYear {
+    private int year;
+    private double subscriptions;
+
+    /**
+     * Constructs a SubscriptionYear object with the specified year label and subscription data.
+     * @param newYear                   the year for a subscription data.
+     * @param newSubscriptions                  the number of subscriptions for a specific year.
+     */
+    public SubscriptionYear(int newYear, double newSubscriptions){
+        year = newYear;
+        subscriptions = newSubscriptions;
+    }
+
+    /**
+     * Sets the value for year to "newYear".
+     * @param newYear                   the input value for year.
+     */
+    public void setYear(int newYear){
+        year = newYear;
+    }
+
+    /**
+     * Sets the value for subscriptions to "newSubscriptions".
+     * @param newSubscriptions                  the input value for subscriptions.
+     */
+    public void setSubscriptions(int newSubscriptions){
+        subscriptions = newSubscriptions;
+    }
+
+    /**
+     * Returns the value of year.
+     * @return the year for a subscription data.
+     */
+    public int getYear(){
+        return year;
+    }
+
+    /**
+     * Returns the value of subscriptions.
+     * @return the number of subscriptions for a specific year.
+     */
+    public double getSubscriptions(){
+        return subscriptions;
+    }
+
+    /**
+     * Returns the only the number of subscriptions.
+     * @return  the number of subscriptions for a specific year.
+     */
+    public String toString(){
+        return String.format("%.2f",subscriptions);
+    }
+}
